3c4d9f392e1563b7916899c741e9d11c6944e37d
Krutik Bundela <krutik@weetechsolution.com>
2025-12-31 12:00:19 +0530

    feat: made commission component dynamic and add new title description of other sections

--- src/components/landing/Commission.tsx
+++ src/components/landing/Commission.tsx
@@ -1,44 +1,79 @@
 import { Stack, Typography } from "@mui/material";
-import People from "../../assets/people.png";
 import dash from "../../assets/green-dash.png";
 import Diamond from "../../assets/green-diamond.svg";
 import GreenArrowButton from "../common/GreenArrowButton";
-export default function Commission() {
+
+interface CommissionProps {
+  title1: string;
+  title2?: string;
+  img: string;
+  width: string;
+  height: string;
+  subtitle: string;
+  desc: string;
+  bg?: string;
+  reverse?: boolean;
+}
+
+export default function Commission({
+  title1,
+  title2,
+  img,
+  width,
+  height,
+  subtitle,
+  desc,
+  bg,
+  reverse,
+}: CommissionProps) {
   return (
     <Stack
-      className="w-100 bg-white justify-center items-center"
+      className={`w-100 ${bg || "bg-white"} justify-center items-center`}
       sx={{ height: "754px" }}
     >
       <Stack
-        className="bg-white  flex-row"
-        sx={{ height: "518px", width: "1192px" }}
+        className={`${reverse ? "flex-row-reverse" : "flex-row"}`}
+        sx={{ height: "518px", width: "1192px", gap: "80px" }}
       >
-        <img src={People} alt="" width="554px" height="518px" />
-        <Stack className="w-100 h-100 justify-start items-start">
+        <img src={img} alt="" width={width} height={height} />
+        <Stack
+          className={`w-100 h-100 ${
+            reverse ? "items-end" : "justify-start items-start"
+          }`}
+        >
           <Typography className="fw-700 fs-44">
-            EARN 30% COMMISSION
+            {title1}
             <br />
-            <span>ON ALL SALES!</span>
+            {title2 && <span>{title2}</span>}
           </Typography>
           <img src={dash} alt="" width={"37px"} />
           <Stack
-            className="w-100 h-100 justify-start items-start flex-row"
+            className={`w-100 h-100 ${
+              reverse
+                ? "flex-row-reverse justify-end"
+                : "flex-row justify-start items-start"
+            }`}
             mt={5}
             sx={{ gap: "21.49px" }}
           >
             <img src={Diamond} alt="" width={"44px"} height={"50px"} />
             <Stack
-              className="w-100 h-100 justify-start items-start"
+              className={`w-100 h-100 ${
+                reverse ? "justify-end items-end" : "justify-start items-start"
+              }`}
               sx={{ gap: "18px" }}
             >
-              <Typography className="fw-700 fs-18">
-                PLUS DONATIONS FROM YOUR SUPPORTERS
+              <Typography
+                className={`fw-700 fs-18 ${reverse ? "text-right" : ""}`}
+              >
+                {subtitle}
               </Typography>
-              <Typography className="fw-500 fs-16 text-gray">
-                Welcome to our studio! We employ people who have passion for
-                design and marketing. It’s a perfect combination of
-                professionalism and creative individual approach to every client
-                or project.
+              <Typography
+                className={`fw-500 fs-16 text-gray ${
+                  reverse ? "text-right" : ""
+                }`}
+              >
+                {desc}
               </Typography>
               <GreenArrowButton />
             </Stack>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,13 +2,48 @@ import Navbar from "../components/landing/Navbar";
 import Header from "../components/landing/Header";
 import EfundersWorkFlow from "../components/landing/EfundersWorkFlow";
 import Commission from "../components/landing/Commission";
+import People from "../assets/people.png";
+import OrderReceived from "../assets/order-received.png";
+import CheeringPeople from "../assets/cheering-people.png";
+
 export default function Landing() {
   return (
     <>
-    <Navbar />
-    <Header/>
-    <EfundersWorkFlow/>
-    <Commission/>
+      <Navbar />
+      <Header />
+      <EfundersWorkFlow />
+      <Commission
+        title1="EARN 30% COMMISSION"
+        title2="ON ALL SALES!"
+        img={People}
+        width="554px"
+        height="518px"
+        subtitle="PLUS DONATIONS FROM YOUR SUPPORTERS"
+        desc="Welcome to our studio! We employ people who have passion for design and marketing. It’s a perfect combination of professionalism and creative individual approach to every client or project."
+      />
+      <Commission
+        title1="NO DOOR TO DOOR SELLING"
+        title2="OR DELIVERIES"
+        img={OrderReceived}
+        width="554px"
+        height="518px"
+        subtitle="SHARE YOUR FUNDRAISER AND WE TAKE CARE OF THE REST!"
+        desc="Welcome to our studio! We employ people who have passion for 
+design and marketing. It’s a perfect combination of professionalism 
+and creative individual approach to every client or project."
+      />
+      <Commission
+        title1="FUNDRAISING FUN!"
+        img={CheeringPeople}
+        width="554px"
+        height="518px"
+        subtitle="WIN ADDITIONAL PRIZES WHEN YOU REACH YOUR FUNDRAISING MILESTONES"
+        desc="Welcome to our studio! We employ people who have passion for
+design and marketing. It’s a perfect combination of professionalism
+and creative individual approach to every client or project."
+        bg="bg-lightgreen"
+        reverse={true}
+      />
     </>
-  )
+  );
 }
